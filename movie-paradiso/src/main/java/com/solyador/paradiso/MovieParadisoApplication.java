d30b15b8451dafe34aaa2e0edfa4a4f09561ae5d
toufikbakhtaoui <toufik.bakhtaoui@gmail.com>
2023-01-16 23:13:14 +0100

    setup project with movie module and swagger

---
+++ movie-paradiso/src/main/java/com/solyador/paradiso/MovieParadisoApplication.java
@@ -0,0 +1,11 @@
+package com.solyador.paradiso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieParadisoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MovieParadisoApplication.class, args);
+    }
+}
